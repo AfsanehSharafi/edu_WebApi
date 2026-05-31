4cc42f03e631ee23a3cc73ee7c691050cfe94761
AfsanehSharafi <ashdev.ir@gmail.com>
2026-05-31 11:47:32 +0330

    third commit

--- Application/Features/Orders/Handlers/Commands/CreateOrderCommandHandler.cs
+++ Application/Features/Orders/Handlers/Commands/CreateOrderCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Contracts;
 using Application.Features.Orders.Requests.Commands;
+using Domain.Entities;
 using MediatR;
 using Microsoft.Extensions.Logging;
 
@@ -36,10 +37,43 @@ namespace Application.Features.Orders.Handlers.Commands
             }
 
             var user = await _userRepository.GetByIdAsync(request.UserId);
-            if(user == null)
+            if (user == null)
             {
                 _logger.LogError("CreateOrderCommand failed: User with ID {UserId} not found.", request.UserId);
                 throw new KeyNotFoundException($"User with ID {request.UserId} not Found.");
+            }
+
+            decimal totalPrice = 0;
+            var orderItemsToAdd = new List<OrderItems>();
+            var order = new Order
+            {
+                UserId= request.UserId,
+                OrderDate = DateTime.Now,
+                Status = OrderStatus.Pending,
+                TotalPrice =0
+            };
+            foreach( var itemDto in request.Items )
+            {
+                if(itemDto.Quantity <= 0)
+                {
+                    _logger.LogWarning("CreateOrderCommand failed: Invalid quantity for ProductId {ProductId}. UserId: {UserId}", itemDto.ProductId, request.UserId);
+                    throw new ArgumentException($"Quantity must be greater than zero for Product ID {itemDto.ProductId}.");
+                }
+
+                var product = await _productRepository.GetByIdAsync(itemDto.ProductId); ;
+                if(product == null)
+                {
+                    _logger.LogError("CreateOrderCommand failed: Product with ID {ProductId} not found. UserId: {UserId}", itemDto.ProductId, request.UserId);
+                    throw new KeyNotFoundException($"Product with ID {itemDto.ProductId} not found.");
+                }
+                var orderItem = new OrderItems
+                {
+                    ProductId =itemDto.ProductId,
+                    Quantity = itemDto.Quantity,
+                    Price = product.Price,
+                };
+                totalPrice += product.Price * itemDto.Quantity;
+
             }
         }
     }
--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d863f5374d0f67488d5906f969dc157054b4c5e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9306a2c4d8d2e0cd84cd42a8c83c7ca77eeea3b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
